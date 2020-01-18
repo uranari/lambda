ac54cd26fa3dc7acddee8e83d737dcb5cd126e8e
uranari <uranaricb@gmail.com>
2020-01-18 20:52:20 +0900

    event load json format

--- slack-notify/main.py
+++ slack-notify/main.py
@@ -1,4 +1,5 @@
 import create_message
+import json
 import os
 import requests
 
@@ -6,19 +7,21 @@ SLACK_WEBHOOK_URL = os.environ.get('slack_alert_url')
 
 
 def lambda_handler(event, context):
-    event_name = event['Records'][0]['Sns']['Message']["eventName"]
+    content = json.loads(event['Records'][0]['Sns']['Message'])
+    event_name = content["eventName"]
 
     if event_name == "ConsoleLogin":
-        user_name = event['Records'][0]['Sns']['Message']["userIdentity"]["userName"]
-        source_ip = event['Records'][0]['Sns']['Message']["sourceIPAddress"]
-        result = event['Records'][0]['Sns']['Message']["responseElements"]["ConsoleLogin"]
+        user_name = content["userIdentity"]["userName"]
+        source_ip = content["sourceIPAddress"]
+        result = content["responseElements"]["ConsoleLogin"]
         message = create_message.console_login_message(
             user_name, source_ip, result)
 
     else:
         message = "Unknown event with " + event_name
 
-    headers = {'Content-Type': 'text/plain'}
+        headers = {'Content-Type': 'text/plain'}
 
-    response = requests.post(SLACK_WEBHOOK_URL, headers=headers, data=message)
-    print(str(response.status_code))
+        response = requests.post(
+            SLACK_WEBHOOK_URL, headers=headers, data=message)
+        print(str(response.status_code))
